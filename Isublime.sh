2b639d5647f6d7434dbc71c5ad5da1319cc57b2b
Dale Corns <drcorns@hotmail.com>
2014-05-09 09:26:37 -0700

    Uncomment testing comments

--- Isublime.sh
+++ Isublime.sh
@@ -1,45 +1,45 @@
 #sublime-text3 install with package control requires argument containing path to tarball
 
 echo 'Installing sublime text'
-#sudo yum -y install wget
-#change this line to latest or desired tarball locations
-#sudo wget c758482.r82.cf2.rackcdn.com/sublime_text_3_build_3059_x64.tar.bz2
+sudo yum -y install wget
+change this line to latest or desired tarball locations
+sudo wget c758482.r82.cf2.rackcdn.com/sublime_text_3_build_3059_x64.tar.bz2
 #change this line to match the name of the tarball
-#subl=sublime_text_3_build_3059_x64.tar.bz2
-#sudo tar jxvf "$subl"
-#sudo rm "$subl"
+subl=sublime_text_3_build_3059_x64.tar.bz2
+sudo tar jxvf "$subl"
+sudo rm "$subl"
 #if the directory created by extraction changes, so should this line
 #         copy launcher for sublime
-#sudo cp sublime_text_3/sublime_text.desktop /usr/share/applications/
+sudo cp sublime_text_3/sublime_text.desktop /usr/share/applications/
 
 #rename sublime and move to /opt
-#sudo mv -f sublime_text_3 /opt/sublime
+sudo mv -f sublime_text_3 /opt/sublime
 
 #       Make Sublime Executable from Terminal
-#cd /usr/local/sbin
-#sudo ln -s /opt/sublime/sublime_text subl
+cd /usr/local/sbin
+sudo ln -s /opt/sublime/sublime_text subl
 
 #       Set Sublime user preferences
-#sudo mkdir -p ~/.config/sublime-text-3/Packages/User
-#sudo mkdir -p ~/.config/sublime-text-3/'Installed Packages'
-#sudo chmod 777 ~/.config/sublime-text-3/Packages/User
-#cd ~/.config/sublime-text-3/Packages/User
-#sudo echo touch Preferences.sublime-settings
-#sudo echo '{' >> Preferences.sublime-settings
-#sudo echo '"ensure_newline_at_eof_on_save" '': true,' >> Preferences.sublime-settings
-#sudo echo '"save_on_focus_lost" '': true,' >> Preferences.sublime-settings
-#sudo echo '"tab_size" '': 2,' >> Preferences.sublime-settings
-#sudo echo '"translate_tabs_to_spaces" '': true,' >> Preferences.sublime-settings
-#sudo echo '"trim_trailing_white_space_on_save" '': true' >> Preferences.sublime-settings
-#sudo echo '}' >> Preferences.sublime-settings
+sudo mkdir -p ~/.config/sublime-text-3/Packages/User
+sudo mkdir -p ~/.config/sublime-text-3/'Installed Packages'
+sudo chmod 777 ~/.config/sublime-text-3/Packages/User
+cd ~/.config/sublime-text-3/Packages/User
+sudo echo touch Preferences.sublime-settings
+sudo echo '{' >> Preferences.sublime-settings
+sudo echo '"ensure_newline_at_eof_on_save" '': true,' >> Preferences.sublime-settings
+sudo echo '"save_on_focus_lost" '': true,' >> Preferences.sublime-settings
+sudo echo '"tab_size" '': 2,' >> Preferences.sublime-settings
+sudo echo '"translate_tabs_to_spaces" '': true,' >> Preferences.sublime-settings
+sudo echo '"trim_trailing_white_space_on_save" '': true' >> Preferences.sublime-settings
+sudo echo '}' >> Preferences.sublime-settings
 #       Sublime Package Control Install
 # Note that google chrome will open to download the package, you must manually close, also if you have yet run google the first time their might be a problem downloading if this is run first
 
-#google-chrome 'https://sublime.wbond.net/Package%20Control.sublime-package'
-#sleep 5s
-#cd ~/Downloads
+google-chrome 'https://sublime.wbond.net/Package%20Control.sublime-package'
+sleep 5s
+cd ~/Downloads
 sudo chmod 777 ~/.config/sublime-text-3 -R
-#sudo mv 'Package Control.sublime-package' ~/.config/sublime-text-3/'Installed Packages/'
+sudo mv 'Package Control.sublime-package' ~/.config/sublime-text-3/'Installed Packages/'
 #       Make Sublime package File
 cd ~/.config/sublime-text-3/Packages/User
 touch 'Package Control.sublime-settings'
